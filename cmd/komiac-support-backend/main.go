89ee788b38be66f4cd463e08970acb48f9de905d
PedrodeParquez <pedrodemash@gmail.com>
2026-02-16 23:05:35 +0700

    initial commit

---
+++ cmd/komiac-support-backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/api/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  200,
+			"message": "Backend is running 🚀",
+		})
+	})
+
+	router.Run(":8080")
+}
